075767faf64a329577e376b225323da9743955a4
a-stephen <sadjignon015@gmail.com>
2025-08-23 12:18:32 +0000

    base struct for the parser

--- src/pdf/parser.zig
+++ src/pdf/parser.zig
@@ -1,1 +1,16 @@
 // Pdf file parser
+
+// I have a sequence of bytes
+
+const std = @import("std");
+
+
+pub const PdfContentParser = struct {
+    fileContent = std.ArrayList([]u8);
+
+
+    pub fn readData(self) []u8 {
+        return self.fileContent.items[0];
+    }
+}
+
